6e1789dbf1c7ea914f95212aa0c22254819adb2c
erp12 <erp12@hampshire.edu>
2016-02-06 10:19:29 -0500

    Added code to plot bus locations on google maps

---
+++ HackPVTA_GoogleMaps.R
@@ -0,0 +1,26 @@
+library('plyr')
+library('dplyr')
+library(RgoogleMaps)
+
+# Find average lon and lat
+meanLat <- mean( PVTA.God.Mode.Data$Latitude)
+meanLon <- mean( PVTA.God.Mode.Data$Longitude)
+
+# Find min lat and lon
+minLat <- min(PVTA.God.Mode.Data$Latitude)
+minLon <- min(PVTA.God.Mode.Data$Longitude)
+
+# Find max lon and lat
+maxLat <- max(PVTA.God.Mode.Data$Latitude)
+maxLon <- max(PVTA.God.Mode.Data$Longitude)
+
+# Turns out it is possible for Lat and Lon can be null
+# I am sure this messes up the averages, but meh.
+
+# Get map that includes all of PVTA area
+PVTA.God.Mode.Map <- GetMap(c(meanLat, meanLon), destfile = "Map1.png", NEWMAP = TRUE, zoom=12, maptype="roadmap")
+
+PVTA.God.Mode.38.Data <- filter(PVTA.God.Mode.Data, Route==38)
+
+# Test the plotting
+PlotOnStaticMap(PVTA.God.Mode.Map, PVTA.God.Mode.38.Data$Latitude[1:100], PVTA.God.Mode.38.Data$Longitude[1:100], col='red')
---
+++ HackPVTA_dataPreprocessing.R
@@ -0,0 +1,24 @@
+library('plyr')
+library('dplyr')
+
+# Vertical Concatination of every month
+#Year2015 <- rbind(January2015,February2015,March2015,April2015,May2015,June2015,July2015,August2015,September2015,October2015,November2015, December2015)
+
+# Grabbing relevant columns
+PVTA.God.Mode.Data <- select(Year2015, V1, V2, V3, V4, V5, V6, V9, V11)
+
+# Renaming the God.Mode columns
+PVTA.God.Mode.Data <- rename(PVTA.God.Mode.Data, Vehicle.ID = V1)
+PVTA.God.Mode.Data <- rename(PVTA.God.Mode.Data, Time = V2)
+PVTA.God.Mode.Data <- rename(PVTA.God.Mode.Data, Latitude = V3)
+PVTA.God.Mode.Data <- rename(PVTA.God.Mode.Data, Longitude = V4)
+PVTA.God.Mode.Data <- rename(PVTA.God.Mode.Data, Speed = V5)
+PVTA.God.Mode.Data <- rename(PVTA.God.Mode.Data, Direction = V6)
+PVTA.God.Mode.Data <- rename(PVTA.God.Mode.Data, Operational.Status = V9)
+PVTA.God.Mode.Data <- rename(PVTA.God.Mode.Data, Route = V11)
+
+# # # # # # # # # # #
+# Utility Functions #
+# # # # # # # # # # #
+
+# None yet :)
