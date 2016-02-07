99716c826f05865ce485875b401c2cd2cdaa9898
erp12 <erp12@hampshire.edu>
2016-02-07 12:34:24 -0500

    Fixing Shiny server errors

--- HackPVTA_App/server.R
+++ HackPVTA_App/server.R
@@ -4,7 +4,7 @@ library(RgoogleMaps)
 
 # current.time.datetime <- as.POSIXct(sort(PVTA.God.Mode.38.Data$Time)[1], tz="EST")
 # current.time <- as.numeric(current.time.datetime)
-current.time <- PVTA.God.Mode.Data$Time[1]
+current.time <- as.numeric(PVTA.God.Mode.38.Data$Time[1])
 
 shinyServer( function(input, output) {
   
@@ -15,42 +15,54 @@ shinyServer( function(input, output) {
   observeEvent(input$btn.back.day, {
     current.time <- current.time - 8.64e+7;
     output$lbl.time <- renderText({current.time})
+    output$map <- renderPlot({
+      current.busses <- filter(PVTA.God.Mode.38.Data, Time > current.time - 100 & Time < current.time + 100)
+      PlotOnStaticMap(PVTA.God.Mode.Map, current.busses$Latitude, current.busses$Longitude, col='red', type="p")
+    })
   })
   observeEvent(input$btn.back.hour, {
     current.time <- current.time - 3.6e+6;
     output$lbl.time <- renderText({current.time})
+    output$map <- renderPlot({
+      current.busses <- filter(PVTA.God.Mode.38.Data, Time > current.time - 100 & Time < current.time + 100)
+      PlotOnStaticMap(PVTA.God.Mode.Map, current.busses$Latitude, current.busses$Longitude, col='red', type="p")
+    })
   })
   observeEvent(input$btn.back.minute, {
     current.time <- current.time - 60000;
+    output$map <- renderPlot({
+      current.busses <- filter(PVTA.God.Mode.38.Data, Time > current.time - 100 & Time < current.time + 100)
+      PlotOnStaticMap(PVTA.God.Mode.Map, current.busses$Latitude, current.busses$Longitude, col='red', type="p")
+    })
   })
   observeEvent(input$btn.forward.minute, {
     current.time <- current.time + 60000;
     output$lbl.time <- renderText({current.time})
     output$map <- renderPlot({
-      current.busses <- filter(PVTA.God.Mode.38.Data, Time > current.time - 60000 & Time < current.time + 60000)
-      PlotOnStaticMap(PVTA.God.Mode.Map, current.busses$Latitude, current.busses$Longitude, col='red', type="l")
+      current.busses <- filter(PVTA.God.Mode.38.Data, Time > current.time - 100 & Time < current.time + 100)
+      PlotOnStaticMap(PVTA.God.Mode.Map, current.busses$Latitude, current.busses$Longitude, col='red', type="p")
     })
   })
   observeEvent(input$btn.forward.hour, {
     current.time <- current.time + 3.6e+6;
     output$lbl.time <- renderText({current.time})
     output$map <- renderPlot({
-      current.busses <- filter(PVTA.God.Mode.38.Data, Time > current.time - 60000 & Time < current.time + 60000)
-      PlotOnStaticMap(PVTA.God.Mode.Map, current.busses$Latitude, current.busses$Longitude, col='red', type="l")
+      current.busses <- filter(PVTA.God.Mode.38.Data, Time > current.time - 100 & Time < current.time + 100)
+      PlotOnStaticMap(PVTA.God.Mode.Map, current.busses$Latitude, current.busses$Longitude, col='red', type="p")
     })
   })
   observeEvent(input$btn.forward.day, {
     current.time <- current.time + 8.64e+7;
     output$lbl.time <- renderText({current.time})
     output$map <- renderPlot({
-      current.busses <- filter(PVTA.God.Mode.38.Data, Time > current.time - 60000 & Time < current.time + 60000)
-      PlotOnStaticMap(PVTA.God.Mode.Map, current.busses$Latitude, current.busses$Longitude, col='red', type="l")
+      current.busses <- filter(PVTA.God.Mode.38.Data, Time > current.time - 100 & Time < current.time + 100)
+      PlotOnStaticMap(PVTA.God.Mode.Map, current.busses$Latitude, current.busses$Longitude, col='red', type="p")
     })
   })
   
   output$map <- renderPlot({
-    current.busses <- filter(PVTA.God.Mode.38.Data, Time > current.time - 60000 & Time < current.time + 60000)
-    PlotOnStaticMap(PVTA.God.Mode.Map, current.busses$Latitude, current.busses$Longitude, col='red', type="l")
+    current.busses <- filter(PVTA.God.Mode.38.Data, Time > current.time - 100 & Time < current.time + 100)
+    PlotOnStaticMap(PVTA.God.Mode.Map, current.busses$Latitude, current.busses$Longitude, col='red', type="p")
   })
   
   
